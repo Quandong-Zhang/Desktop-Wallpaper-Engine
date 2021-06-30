980b8b86f81ab0195bcd58778721accb586a6b5c
Quandong-Zhang <78526012+Quandong-Zhang@users.noreply.github.com>
2021-06-30 23:06:42 +0800

    Update 2.再运行我实现桌面视频壁纸.py

--- 2.再运行我实现桌面视频壁纸.py
+++ 2.再运行我实现桌面视频壁纸.py
@@ -1,14 +1,14 @@
 import ctypes
 import time
 import os
-cln=1
-shijian=0.19
+pic_num=1
+times=0.19
 path=os.getcwd()
 while True:
-  paths=path+".\pic\image"+str(cln)+".jpg"
+  paths=path+".\pic\image"+str(pic_num)+".jpg"
   if os.path.exists(paths):
     ctypes.windll.user32.SystemParametersInfoW(20, 0, paths, 0)  # 设置桌面
-    time.sleep(shijian)
-    cln+=1
+    time.sleep(times)
+    pic_num+=1
   else:
-    cln=1
+    pic_num=1
